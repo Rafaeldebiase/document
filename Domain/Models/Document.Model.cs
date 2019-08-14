ae7d7761606f6f30ae5a4d8593758caedc58f3f4
Rafael de biase <rafaelvitaldebiase@gmail.com>
2019-08-13 23:09:24 -0300

    Cria o document model

--- Domain/Models/Document.Model.cs
+++ Domain/Models/Document.Model.cs
@@ -1,24 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using Document.Enum;
 using Document.ObjectValue;
 
 namespace Document.Domain
 {
-    public class Document
+    public class DocumentModel
     {
-        public Document(Code code, string title, string process, Category category, string archive)
+        public DocumentModel()
+        {
+            
+        }
+        public DocumentModel(Code code, string title, string process, Category category, byte[] archive)
         {
             Code = code;
             Title = title;
             Process = process;
             Category = category;
             Archive = archive;
+
         }
 
+        public Guid Id { get; private set; }
         public Code Code { get; private set; }
         public string Title { get; private set; }
         public string Process { get; private set; }
         public Category Category { get; private set; }
-        public string Archive { get; private set; }
+        public byte[] Archive { get; private set; }
     }
 }
 
