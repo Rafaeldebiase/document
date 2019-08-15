fa7d7da512402e5480b76b75170f5a1ac1724616
Rafael de Biase <rafael.biase@opentechgr.com.br>
2019-08-15 13:46:05 -0300

    implementa regra de delecao

--- Domain/Models/Document.Model.cs
+++ Domain/Models/Document.Model.cs
@@ -1,15 +1,14 @@
-using System;
-using System.ComponentModel.DataAnnotations;
+using Document.Dto;
 using Document.Enum;
-using Document.ObjectValue;
+
 
 namespace Document.Domain
 {
     public class DocumentModel
     {
-        public DocumentModel()
+        public DocumentModel(bool delete = false)
         {
-            
+            Delete = delete;
         }
 
         public DocumentModel(int code, string title, string process, Category category, byte[] archive, 
@@ -29,6 +28,22 @@ namespace Document.Domain
         public Category Category { get; private set; }
         public byte[] Archive { get; private set; }
         public bool Delete { get; private set; }
+
+        public DocumentDto ConvertToDocumentDto(DocumentModel document)
+        {
+            return new DocumentDto(document.Code, document.Title, document.Process,
+                (int)document.Category, document.Delete);
+        }
+
+        public DocumentModel Deleted(DocumentModel documentModel)
+        {
+            var documentDto = documentModel.ConvertToDocumentDto(documentModel);
+
+            documentDto.Delete = true;
+
+            return new DocumentModel(documentDto.Code, documentDto.Title, documentDto.Process,
+                documentDto.Category, documentDto.Archive, documentDto.Delete);
+        }
     }
 }
 
