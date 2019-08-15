99481392520db28bee2cd82d120ed52aeed507f8
Rafael de Biase <rafael.biase@opentechgr.com.br>
2019-08-15 18:31:13 -0300

    inclui metodos

--- IServices/IDocumentService.cs
+++ IServices/IDocumentService.cs
@@ -1,11 +1,13 @@
 using System.Threading.Tasks;
 using Document.Domain;
-
+using Microsoft.AspNet.OData;
 
 namespace Document.Interface.Service
 {
     public interface IDocumentService
     {
         Task<int> Insert(DocumentModel document);
+        Task<DocumentModel> GetId(int code);
+        Task Edit(Delta<DocumentModel> document, DocumentModel entity);
     }
 }
