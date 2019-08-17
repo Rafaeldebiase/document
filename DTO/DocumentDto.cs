ae1eacabd1be96b97c2dbc83a65d5b5d6bd1e182
Rafael de biase <rafaelvitaldebiase@gmail.com>
2019-08-17 14:07:23 -0300

    refatoração

--- DTO/DocumentDto.cs
+++ DTO/DocumentDto.cs
@@ -1,62 +1,61 @@
 using Document.Domain;
 using Document.Enum;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Document.Dto
 {
     public class DocumentDto
     {
-        public DocumentDto()
-        {
-            
-        }
-        public DocumentDto(int code, string title, string process, int category, bool delete)
-        {
-            Code = code;
-            Title = title;
-            Process = process;
-            Category = ReceiveIntAndReturnCategory(category);
-            Archive = new byte[0];
-            Delete = delete;
-        }
-
         public int Code { get; set; }
         public string Title { get; set; }
         public string Process { get; set; }
-        public Category Category { get; set; }
-        public byte[] Archive { get; set; }
+        public int Category { get; set; }
+        public byte[] File { get; set; }
+        public IFormFile FormFile { get; set; }
         public bool Delete { get; set; }
 
-        private Category ReceiveIntAndReturnCategory(int category)
+        private async Task<byte[]> UploadAsync(IFormFile file)
         {
-            if ((int)Category.RN1 == category)
+            using (var memoryStream = new MemoryStream())
             {
-                return Category.RN1;
-            }
-            if ((int)Category.RN2 == category)
-            {
-                return Category.RN2;
-            }
-            if ((int)Category.RN3 == category)
-            {
-                return Category.RN3;
-            }
-            if ((int)Category.RN4 == category)
-            {
-                return Category.RN4;
-            }
-            else
-            {
-                return Category.RN5;
+                await file.CopyToAsync(memoryStream);
+                return memoryStream.ToArray();
             }
         }
 
-        public DocumentModel ConvertToDocumentModel(DocumentDto documentDto)
-        {
-            return new DocumentModel(documentDto.Code, documentDto.Title, documentDto.Process, 
-                documentDto.Category, documentDto.Archive, documentDto.Delete);
-        }
+        // private Category ReceiveIntAndReturnCategory(int category)
+        // {
+        //     if ((int)Category.RN1 == category)
+        //     {
+        //         return Category.RN1;
+        //     }
+        //     if ((int)Category.RN2 == category)
+        //     {
+        //         return Category.RN2;
+        //     }
+        //     if ((int)Category.RN3 == category)
+        //     {
+        //         return Category.RN3;
+        //     }
+        //     if ((int)Category.RN4 == category)
+        //     {
+        //         return Category.RN4;
+        //     }
+        //     else
+        //     {
+        //         return Category.RN5;
+        //     }
+        // }
+
+        // public DocumentModel ConvertToDocumentModel(DocumentDto documentDto)
+        // {
+        //     return new DocumentModel(documentDto.Code, documentDto.Title, documentDto.Process, 
+        //         documentDto.Category, documentDto.Archive, documentDto.Delete);
+        // }
     }
 }
